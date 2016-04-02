bc895536e2a682205281d11d4a95ac28891ba5e9
ProfesorSergio <profesorsergio@A51-MM-14.local>
2016-04-02 12:08:08 -0500

    Clase02

--- Clase 02/02-autenticacion/app.js
+++ Clase 02/02-autenticacion/app.js
@@ -14,6 +14,31 @@ function fnFormLogin(req, res) {
 	res.render("login");
 }
 
+function fnLogin(req, res) {
+	var usuario = req.body.usuario,
+		contrasena = req.body.contrasena;
+
+	if(usuario=="sergio" && contrasena=="123"){
+		req.session.user = {usuario: usuario};
+		//res.render("lista");
+		res.redirect("/lista-usuarios");
+	} else {
+		res.redirect("/");
+	}
+}
+
+function fnListaUsuarios(req, res) {
+	res.render("lista");
+}
+
+function fnValidar(req, res, next) {
+	if(req.session.user==null) {
+		res.redirect("/");
+	} else {
+		next();
+	}
+}
+
 app.use(cookieParser());
 app.use(cookieSession({secret: "123456"}));
 app.use(bodyParser.json());
@@ -25,6 +50,8 @@ app.set("views", dirVistas);
 app.set("view engine", motorVistas);
 
 app.get("/", fnFormLogin);
+app.post("/", fnLogin);
+app.get("/lista-usuarios", fnValidar, fnListaUsuarios);
 app.listen(3000, fnEjecutando);
 
 
